6d95e9202b1b6a6ffb668ce29be7dbd460ac8bf8
saraalhindi7 <saraalmakkawi@gmail.com>
2018-10-06 08:49:22 +0300

    add match - not complete

---
+++ app/src/main/java/com/example/hanaalalawi/betsapp/AddMatchFragment.java
@@ -0,0 +1,54 @@
+package com.example.hanaalalawi.betsapp;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class AddMatchFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_add_match, container, false);
+
+        Button dateButton = (Button) v.findViewById(R.id.dateButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getFragmentManager(),"date picker");
+
+            }
+        });
+        return inflater.inflate(R.layout.fragment_add_match,container,false );
+    }
+
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR,year);
+        c.set(Calendar.MONTH,month);
+        c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        TextView dateTextView = (TextView) view.findViewById(R.id.dateText);
+        dateTextView.setText(currentDateString);
+
+    }
+}
--- app/src/main/java/com/example/hanaalalawi/betsapp/MainActivity.java
+++ app/src/main/java/com/example/hanaalalawi/betsapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.hanaalalawi.betsapp;
 
+import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.FragmentManager;
@@ -10,22 +12,29 @@ import android.app.FragmentTransaction;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    Button HomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottobNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navList);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_place,new HomeFragment()).commit();
     }
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener navList =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
@@ -36,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                             selectedFragment = new HomeFragment();
                             break;
                         case R.id.nav_addMatch:
-                            selectedFragment = new ProfileFragment();
+                            selectedFragment = new AddMatchFragment();
                             break;
                         case R.id.nav_result:
                             selectedFragment = new ResultFragment();
