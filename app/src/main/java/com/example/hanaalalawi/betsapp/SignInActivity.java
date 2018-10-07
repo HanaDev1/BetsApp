b4e6daf81e7b53887a18cb08fbbb4b40fdf38be5
HanaDev1 <hanaalalwi1@gmail.com>
2018-10-07 12:17:45 +0300

    User and Admin Login

--- app/src/main/java/com/example/hanaalalawi/betsapp/SignInActivity.java
+++ app/src/main/java/com/example/hanaalalawi/betsapp/SignInActivity.java
@@ -15,13 +15,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignInActivity extends AppCompatActivity {
     TextView toSignUp;
     EditText userEmailInput, userPassInput;
-    String userEmail, userPassword;
     FirebaseAuth mAuth;
     Button signInBtn;
 
@@ -38,12 +40,9 @@ public class SignInActivity extends AppCompatActivity {
         toSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity( new Intent(SignInActivity.this,UserSignUpActivity.class));
+                startActivity(new Intent(SignInActivity.this, UserSignUpActivity.class));
             }
         });
-
-        userEmail = userEmailInput.getText().toString().trim();
-        userPassword = userPassInput.getText().toString().trim();
 
         mAuth = FirebaseAuth.getInstance();
         signInBtn.setOnClickListener(new View.OnClickListener() {
@@ -52,31 +51,57 @@ public class SignInActivity extends AppCompatActivity {
                 signIn();
             }
         });
-
     }
+    public void signIn() {
+        //Admin email is betsAdmin@gmail.com and the password is 123456
+        final String userEmail = userEmailInput.getText().toString().trim();
+        String userPassword = userPassInput.getText().toString().trim();
 
-    public void signIn(){
-
-        if (TextUtils.isEmpty(userEmail)){
+        if (TextUtils.isEmpty(userEmail)) {
             userEmailInput.setError("Enter your email");
             userEmailInput.requestFocus();
+            return;
         }
-        if(TextUtils.isEmpty(userPassword)){
+        if (TextUtils.isEmpty(userPassword)) {
             userPassInput.setError("Enter password");
             userPassInput.requestFocus();
+            return;
         }
-        mAuth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAuth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(!task.isSuccessful()){
-                    Toast.makeText(getApplicationContext(),"Please check your email or password",Toast.LENGTH_LONG);
-                }else{
-                    //
-                    Intent toHomePage = new Intent(SignInActivity.this,MainActivity.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putString("user_email", userEmail);
-                    toHomePage.putExtras(bundle);
-                    startActivity(toHomePage);
+                if (!task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Please check your email or password", Toast.LENGTH_LONG);
+                } else {
+
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("User");
+                    reference.orderByChild("user_email").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot userSnap : dataSnapshot.getChildren()) {
+                                // admin Login
+                                if (userSnap.child("user_email").getValue(String.class).equals("betsAdmin@gmail.com")) {
+                                    Intent intent = new Intent(SignInActivity.this, AdminHomeActivity.class);
+                                    Bundle bundle = new Bundle();
+                                    bundle.putString("user_email", userEmail);
+                                    intent.putExtras(bundle);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                } else {
+                                    // Other users Login
+                                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                                    Bundle bundle = new Bundle();
+                                    bundle.putString("user_email", userEmail);
+                                    intent.putExtras(bundle);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                }
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                        }
+                    });
                 }
             }
         });
